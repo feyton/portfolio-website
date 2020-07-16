0c1200257f981a330736d39b684d4303681235c3
feyton <tumbafabruce@gmail.com>
2020-07-16 12:04:52 +0200

    Edited detail view

--- feyton/settings/local.py
+++ feyton/settings/local.py
@@ -7,12 +7,15 @@ ALLOWED_HOSTS = []
 
 SECRET_KEY = config('SECRET_KEY')
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'HOST': 'localhost',
+        'PASSWORD': 'admin',
+        'USER': 'postgres'
+    }
+}
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST_USER = 'no-reply@feyton.co.rw'
